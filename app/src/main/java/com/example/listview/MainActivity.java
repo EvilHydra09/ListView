08fbfb9cec208df6b55a7b001f606bbffd563642
abhidnya <abhidnya09@gmail.com>
2022-07-15 12:22:01 +0530

    List View Is Completed

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+ListView listView;
+String arr[] = {"Mumbai","Pune","Pimpri Chinchwad","Lavasa","Banglore","Chennai","Rajasthan"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.list);
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_dropdown_item_1line,arr);
+        listView.setAdapter(ad);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked "+arr[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
